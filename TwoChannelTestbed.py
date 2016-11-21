dd998cb9713e44c0c1d0fd302783c6897b5d9180
Marius Kriegerowski <marius@gfz-potsdam.de>
2016-11-21 08:52:08 +0100

    whitespaces cleanup

--- TwoChannelTestbed.py
+++ TwoChannelTestbed.py
@@ -256,11 +256,10 @@ class LiveFFTWidget(QWidget):
         self.freq_vect2 = np.fft.rfftfreq(mic.chunksize,
                                               1./mic.rate)
         self.time_vect2 = np.arange(mic.chunksize, dtype=np.float32) / mic.rate * 1000
-    
+
         self.pitchlog_vect1 = np.arange(PITCHLOGLEN, dtype=np.float32)
-    
+
         self.pitchlog_vect2 = np.arange(PITCHLOGLEN, dtype=np.float32)
-    
 
     def connectSlots(self):
         pass
@@ -275,14 +274,14 @@ class LiveFFTWidget(QWidget):
         self.ax_ch1_time.set_xlabel(u'time (ms)', fontsize=6)
         #pos = [pos1.x0, pos1.y0, pos1.width, pos1.height]
         self.ax_ch1_time.set_position([0.1, 0.8, 0.3, 0.15])
-        
+
         # channel 2 time
         self.ax_ch2_time = self.main_figure.figure.add_subplot(614)
         self.ax_ch2_time.set_ylim(-32768, 32768)
         self.ax_ch2_time.set_xlim(0, self.time_vect1.max())
         self.ax_ch2_time.set_xlabel(u'time (ms)', fontsize=6)
         self.ax_ch2_time.set_position([0.1,0.6, 0.3, 0.15])
-        
+
         # channel 1 spec
         self.ax_ch1_spec = self.main_figure.figure.add_subplot(612)
         #self.ax_ch1_spec.set_ylim(0, 1)
@@ -291,7 +290,7 @@ class LiveFFTWidget(QWidget):
         self.ax_ch1_spec.set_xlim(0, 1000)
         #self.ax_ch1_spec.set_xlabel(u'frequency (Hz)\n', fontsize=6)
         self.ax_ch1_spec.set_position([0.45,0.8, 0.4, 0.15])
-        
+
         # channel 2 spec
         self.ax_ch2_spec = self.main_figure.figure.add_subplot(613)
         #self.ax_ch2_spec.set_ylim(0, 1)
@@ -300,34 +299,30 @@ class LiveFFTWidget(QWidget):
         self.ax_ch2_spec.set_xlim(0, 1000)
         self.ax_ch2_spec.set_xlabel(u'frequency (Hz)\n', fontsize=6)
         self.ax_ch2_spec.set_position([0.45,0.6, 0.4, 0.15])
-        
 
-        
         # gauges
         # PolarAxes.PolarTransform takes radian. However, we want our coordinate
         # system in degree
         tr = Affine2D().scale(np.pi/180., 1.) + PolarAxes.PolarTransform()
-    
+
         # Find grid values appropriate for the coordinate (degree).
         # The argument is an approximate number of grids.
-    
+
         grid_locator1 = angle_helper.LocatorD(2)
         # And also use an appropriate formatter:
         tick_formatter1 = angle_helper.FormatterDMS()
-    
-    
+
         angle_ticks = [(0*180/1200, "8ve"),(100*180/1200, "7M"),(200*180/1200, "7m"),(300*180/1200, "6M"),(400*180/1200, "6m"),(500*180/1200, "5"),
                    (700*180/1200, "4"),(800*180/1200, "3M"),(900*180/1200, "3m"),(1000*180/1200, "2M"),(1100*180/1200, "2m"),(1200*180/1200, "U")]
 
         grid_locator1 = FixedLocator([v for v, s in angle_ticks])
         tick_formatter1 = DictFormatter(dict(angle_ticks))
-                   
-                   
+
         # set up number of ticks for the r-axis
         grid_locator2 = MaxNLocator(1)
         radius_ticks = [(0, ""),(1, "")]
         tick_formatter2 = DictFormatter(dict(radius_ticks))
-                   
+
         # the extremes are passed to the function
         grid_helper = floating_axes.GridHelperCurveLinear(tr,
                                                                      extremes=(0, 180, 0.55, 1.),
@@ -336,37 +331,37 @@ class LiveFFTWidget(QWidget):
                                                                      tick_formatter1=tick_formatter1,
                                                                      tick_formatter2=tick_formatter2
                                                                      )
-        
+
         #self.gauge1 = self.main_figure.figure.add_subplot(514)
         # self.gauge1_ax1 used to be called ax1
         self.gauge1_ax1 = floating_axes.FloatingSubplot(self.main_figure.figure,615, grid_helper=grid_helper)
         self.main_figure.figure.add_subplot(self.gauge1_ax1)
         self.gauge1_ax1.set_position([0.35,0.05, 0.3, 0.3])
         pllabel="Interval (cents)"
-        
+
         # adjust axis
         self.gauge1_ax1.axis["left"].set_axis_direction("bottom")
         self.gauge1_ax1.axis["right"].set_axis_direction("top")
-    
+
         self.gauge1_ax1.axis["bottom"].set_visible(True)
         self.gauge1_ax1.axis["bottom"].set_axis_direction("top")
         self.gauge1_ax1.axis["bottom"].major_ticklabels.set_axis_direction("bottom")
         self.gauge1_ax1.axis["bottom"].label.set_axis_direction("bottom")
-    
+
         self.gauge1_ax1.axis["top"].set_axis_direction("bottom")
         self.gauge1_ax1.axis["top"].toggle(ticklabels=True, label=True)
         self.gauge1_ax1.axis["top"].major_ticklabels.set_axis_direction("top")
         self.gauge1_ax1.axis["top"].label.set_axis_direction("top")
-    
+
         #ax1.axis["left"].label.set_text(r"cz [km$^{-1}$]")
         #self.gauge1_ax1.axis["left"].label.set_text(r"")
         #self.gauge1_ax1.axis["right"].label.set_text(r"")
         self.gauge1_ax1.axis["top"].label.set_text(pllabel)
         #self.gauge1_ax1.axis["bottom"].label.set_text(" ")
-    
+
         # create a parasite axes whose transData in RA, cz
         self.gauge1_aux_ax = self.gauge1_ax1.get_aux_axes(tr)
-    
+
         self.gauge1_aux_ax.patch = self.gauge1_ax1.patch  # for aux_ax to have a clip path as in ax
         self.gauge1_ax1.patch.zorder = 0.9  # but this has a side effect that the patch is
         # drawn twice, and possibly over some other
@@ -374,17 +369,16 @@ class LiveFFTWidget(QWidget):
         # prevent this.
         for an in [100.,200,300,400,500,700,800,900,1000,1100]:
             self.gauge1_aux_ax.plot([an*180/1200, an*180/1200], [0.5,1.], color='black',lw=1,ls='dotted')
-        
+
         patches=[]
         #here I create the wedge to start
         self.gauge1_ax1.gauge1_wedge=Wedge((0,0),0.99,1,120,width=0.38,facecolor='#FF0000')
         self.gauge1_ax1.add_patch(self.gauge1_ax1.gauge1_wedge)
         for an in [100.,200,300,400,500,700,800,900,1000,1100]:
             self.gauge1_aux_ax.plot([an*180/1200, an*180/1200], [0.5,1.], color='black',lw=1,ls='dotted')
-        
+
         # End gauge 1 -----------------------------------------------------------------------------------------------
-        
-        
+
         # pitch log plot
         self.ax_pitchlogs = self.main_figure.figure.add_subplot(616)
         self.ax_pitchlogs.set_ylim(190,1000)
@@ -392,7 +386,7 @@ class LiveFFTWidget(QWidget):
         self.ax_pitchlogs.set_xlabel(u'pitch index ', fontsize=6)
         self.ax_pitchlogs.set_position([0.1,0.4, 0.75, 0.15])
         #self.ax_pitchlogs.set_position([0.1,0.1, 0.8, 0.25])
-        
+
         # mark the 3rd, 4th and fifth
         self.ax_pitchlogs.plot(self.pitchlog_vect1,300 * np.ones_like(self.pitchlog_vect1),color='green',ls='dotted')
         self.ax_pitchlogs.plot(self.pitchlog_vect1,400 * np.ones_like(self.pitchlog_vect1),color='green',ls='dashed')
@@ -400,9 +394,9 @@ class LiveFFTWidget(QWidget):
         self.ax_pitchlogs.plot(self.pitchlog_vect1,700 * np.ones_like(self.pitchlog_vect1),color='black',lw=2,ls='dotted')
         self.ax_pitchlogs.plot(self.pitchlog_vect1,800 * np.ones_like(self.pitchlog_vect1),color='green',ls='dotted')
         self.ax_pitchlogs.plot(self.pitchlog_vect1,900 * np.ones_like(self.pitchlog_vect1),color='green',ls='dashed')
-        
+
         #self.ax_pitchlogs.plot(self.pitchlog_vect1,-700 * np.ones_like(self.pitchlog_vect1),color='black',ls='dotted')
-        
+
         # line objects
         self.line_ch1_time, = self.ax_ch1_time.plot(self.time_vect1,
                                          np.ones_like(self.time_vect1),color='black')
@@ -422,7 +416,7 @@ class LiveFFTWidget(QWidget):
 
         # pitch log
         self.ch2_pitchlog, = self.ax_pitchlogs.plot(self.pitchlog_vect2, np.ones_like(self.pitchlog_vect2),color='red',lw=0,marker="o")
-    
+
         # tight layout
         #plt.tight_layout()
 
@@ -451,14 +445,14 @@ class LiveFFTWidget(QWidget):
             else:
                 fft_frame *= (1 + self.fixedGainSlider.value()) / 5000000.
                 #print(np.abs(fft_frame).max())
-            
+
             #print("{} {}".format(min(np.log(np.abs(fft_frame))),max(np.log(np.abs(fft_frame)))))
             self.line_ch1_spec.set_data(self.freq_vect1, np.log(np.abs(fft_frame)))
-            
+
             #time_str = "Time to plot channel1 in ms: %f" % ((time.time()-t0g)*1000)
             #print("{}".format(time_str))
             #  pitch tracking algorithm
-            
+
             t0g = time.time()
             #new_pitch1 = compute_pitch_hps(current_frame1, self.mic.rate,dF=1)
             #precise_pitch1 = compute_pitch_hps(current_frame1, self.mic.rate, dF=0.05, Fmin=new_pitch1 * 0.8, Fmax = new_pitch1 * 1.2)
@@ -467,8 +461,7 @@ class LiveFFTWidget(QWidget):
             pitch_confidence1 = pitch_o.get_confidence()
             #time_str = "Time to pitch track channel1 in ms: %f" % ((time.time()-t0g)*1000)
             #print("{}".format(time_str))
-            
-            
+
             self.ax_ch1_spec.set_title("pitch = {:.2f} Hz".format(precise_pitch1))
             self.ch1_pitch_line.set_data((new_pitch1, new_pitch1),
                                      self.ax_ch1_spec.get_ylim())
@@ -476,7 +469,7 @@ class LiveFFTWidget(QWidget):
             # channel 2
             # plots the time signal 2
             self.line_ch2_time.set_data(self.time_vect2, current_frame2)
-            
+
             # computes and plots the fft signal
             fft_frame = np.fft.rfft(current_frame2)
             if self.autoGainCheckBox.checkState() == QtCore.Qt.Checked:
@@ -484,13 +477,13 @@ class LiveFFTWidget(QWidget):
             else:
                 fft_frame *= (1 + self.fixedGainSlider.value()) / 5000000.
                 #print(np.abs(fft_frame).max())
-            
+
             #self.line_ch2_spec.set_data(self.freq_vect2, np.abs(fft_frame))
             self.line_ch2_spec.set_data(self.freq_vect1, np.log(np.abs(fft_frame)))
             #  pitch tracking algorithm
             #new_pitch2 = compute_pitch_hps(current_frame2, self.mic.rate,dF=1)
             #precise_pitch2 = compute_pitch_hps(current_frame2, self.mic.rate,dF=0.05, Fmin=new_pitch2 * 0.8, Fmax = new_pitch2 * 1.2)
-            
+
             t0g = time.time()
             signal2float=current_frame2.astype(np.float32)
             new_pitch2=precise_pitch2=pitch_o(signal2float)[0]
@@ -504,7 +497,7 @@ class LiveFFTWidget(QWidget):
 
             new_pitch1Cent = 1200* math.log((new_pitch1+.1)/120.,2)
             new_pitch2Cent = 1200* math.log((new_pitch2+.1)/120.,2)
-                
+
             ivCents=abs(new_pitch2Cent-new_pitch1Cent)
             #t1 = time.time()
             self.live_gauge.update_value(ivCents)
@@ -523,7 +516,7 @@ class LiveFFTWidget(QWidget):
 
             update_pitch_log2(abs(new_pitch2Cent-new_pitch1Cent))
             self.ch2_pitchlog.set_data(self.pitchlog_vect2, pitchlog2)
-        
+
             # refreshes the plots
             self.main_figure.canvas.draw()
             print('total time', time.time()-t1)
