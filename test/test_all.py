d5b142ee2c3bda7e774e2e5ce74b43e185be3403
HerrMuellerluedenscheid <marius@gfz-potsdam.de>
2016-12-08 11:07:17 +0100

    travis

--- test/test_all.py
+++ test/test_all.py
@@ -1,7 +1,7 @@
 import unittest
 
-from test_buffer import *
-from test_mic import *
+from test_buffer import BufferTestCase
+from test_mic import MicTestCase
 
 if __name__=='__main__':
     unittest.main()
