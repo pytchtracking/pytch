a0e109907f6f5e5f035389897e75d8a082288890
HerrMuellerluedenscheid <marius@gfz-potsdam.de>
2017-07-20 14:55:00 +0200

    tct: psd

--- src/two_channel_tuner.py
+++ src/two_channel_tuner.py
@@ -1,12 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import numpy as num
-
-#from pytch.util import f2pitch
-
 import logging
 
-#_standard_frequency = 220.
 logger = logging.getLogger(__name__)
 
 
@@ -17,10 +13,7 @@ class Worker():
 
         :param buffer_length: in seconds'''
 
-        self.ndata_scale = 16*2
         self.channels = channels
-        self.nchannels = len(self.channels)
-        self.cross_spectra_combinations = []
 
     def process(self):
         ''' Do the work'''
@@ -28,8 +21,12 @@ class Worker():
 
         for ic, channel in enumerate(self.channels):
             frame_work = channel.latest_frame_data(channel.fftsize)
+            win = num.hanning(channel.fftsize)
+            # slight pre-emphasis
+            # frame_work[1:] -=  0.1 * frame_work[:-1]
+            # frame_work[0] = frame_work[1]
 
-            amp_spec = num.abs(num.fft.rfft(frame_work))
+            amp_spec = num.abs(num.fft.rfft(frame_work * win)) ** 2 / channel.fftsize
             channel.fft.append(num.asarray(amp_spec, dtype=num.uint32))
 
             channel.pitch_confidence.append_value(
@@ -44,3 +41,26 @@ def cross_spectrum(spec1, spec2):
     ''' Returns cross spectrum and phase of *spec1* and *spec2*'''
     cross = spec1 * spec2.conjugate()
     return num.abs(cross), num.unwrap(num.arctan2(cross.imag, cross.real))
+
+
+def i(pow_frames):
+    ''' based on  http://haythamfayek.com/2016/04/21/speech-processing-for-machine-learning.html'''
+    low_freq_mel = 0
+    high_freq_mel = (2595 * numpy.log10(1 + (sample_rate / 2) / 700))  # Convert Hz to Mel
+    mel_points = numpy.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # Equally spaced in Mel scale
+    hz_points = (700 * (10**(mel_points / 2595) - 1))  # Convert Mel to Hz
+    bin = numpy.floor((NFFT + 1) * hz_points / sample_rate)
+
+    fbank = numpy.zeros((nfilt, int(numpy.floor(NFFT / 2 + 1))))
+    for m in range(1, nfilt + 1):
+        f_m_minus = int(bin[m - 1])   # left
+        f_m = int(bin[m])             # center
+        f_m_plus = int(bin[m + 1])    # right
+
+        for k in range(f_m_minus, f_m):
+            fbank[m - 1, k] = (k - bin[m - 1]) / (bin[m] - bin[m - 1])
+        for k in range(f_m, f_m_plus):
+            fbank[m - 1, k] = (bin[m + 1] - k) / (bin[m + 1] - bin[m])
+    filter_banks = numpy.dot(pow_frames, fbank.T)
+    filter_banks = numpy.where(filter_banks == 0, numpy.finfo(float).eps, filter_banks)  # Numerical Stability
+    filter_banks = 20 * numpy.log10(filter_banks)  # dB
