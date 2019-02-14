56fac56bbdb1d5443f6812a8a4375e58017e3b18
Marius <marius.kriegerowski@gmail.com>
2019-02-14 12:51:42 +0100

    channel mixer started

--- src/level_meter.py
+++ src/level_meter.py
@@ -6,11 +6,13 @@ import sys
 from .gui_util import AdjustableMainWindow
 
 class LevelMeter(qw.QWidget):
+    '''Level meter for a single channel'''
 
-    def __init__(self):
+    def __init__(self, channel, update_interval=0.1):
         super(LevelMeter, self).__init__()
         layout = qw.QVBoxLayout(self)
 
+        self.channel = channel
         self.level = 0.5
 
         self.setStyleSheet('''QWidget {
@@ -21,24 +23,34 @@ class LevelMeter(qw.QWidget):
         title = qw.QLabel('Level')
         layout.addWidget(title)
 
+        self.refresh_timer = qc.QTimer()
+        self.refresh_timer.timeout.connect(1000*self.update)
+        self.refresh_timer.start(self.update_interval)
+
+    def get_level(self):
+        return num.mean(self.channel.latest_frame(self.update_interval))
+
     def paintEvent(self, event):
+        v = self.get_level()
+        logger.debug('new level: %s' % v)
         painter = qg.QPainter(self)
         bar = self.rect()
         padding = 10
-        level = bar.height() - 2*padding - bar.height() * self.level
+        level = bar.height() - 2*padding - bar.height() * v
         bar.setCoords(
             bar.left() + padding, bar.bottom() - padding - level,
             bar.right() - padding, bar.top() + padding,)
         painter.fillRect(bar, self.bar_color)
         painter.end()
 
-    @qc.pyqtSlot(float)
-    def levelChaned(self, v):
-        '''Set the level scaled between 0. and 1.'''
-        logger.debug('new level: %s' % v)
 
-        self.level = v
-        self.update()
+class ChannelMixer(qw.QWidget):
+
+    def __init__(self, channels):
+        super(ChannelMixer, self).__init__()
+        layout = self.qw.QHBoxLayout()
+        for channel in channels:
+            layout.addWidget(LevelMeter(channel=channel))
 
 
 def app_add_widget():
