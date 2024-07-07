3d41c564810f1b86d3eb376344a151d3b90f6565
sebastianrosenzweig <basti.rosenzweig@googlemail.com>
2024-07-07 21:26:27 +0200

    started connecting new audio processor

--- test/test_audio.py
+++ test/test_audio.py
@@ -4,16 +4,16 @@ from pytch.audio import (
     RingBuffer,
     AudioProcessor,
     get_input_devices,
-    get_sampling_rate_options,
+    get_fs_options,
 )
 
 print(get_input_devices())
-print(get_sampling_rate_options(1))
+print(get_fs_options(1))
 
 
 buf = RingBuffer(size=(20, 1), dtype=np.float64)
 buf.write(np.arange(20).reshape(-1, 1))
-test = buf.read(5)
+test = buf.read(20)
 
 #
 ap = AudioProcessor(
@@ -22,7 +22,7 @@ ap = AudioProcessor(
     fft_len=1024,
     hop_len=256,
     blocksize=4096,
-    n_channels=1,
+    channels=[0],
     device_no=1,
     f0_algorithm="YIN",
 )
